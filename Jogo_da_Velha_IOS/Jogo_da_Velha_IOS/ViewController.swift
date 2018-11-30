c2bbc55e32a8f6915863dab78fbe8bfead092c9b
Ednilson Rossi <ednilsonrossi@gmail.com>
2018-11-30 21:12:28 -0200

    Tela com tabuleiro

--- Jogo_da_Velha_IOS/Jogo_da_Velha_IOS/ViewController.swift
+++ Jogo_da_Velha_IOS/Jogo_da_Velha_IOS/ViewController.swift
@@ -5,11 +5,24 @@
 //  Created by pos on 30/11/2018.
 //  Copyright © 2018 pos. All rights reserved.
 //
+//  Desenvolvido por:
+//      André Cremonezi - andrecremonezi7@gmail.com
+//      Ednilson Rossi  - ednilson.rossi@aluno.ifsp.edu.br
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btn1x1: UIButton!
+    @IBOutlet weak var btn1x2: UIButton!
+    @IBOutlet weak var btn1x3: UIButton!
+    @IBOutlet weak var btn2x1: UIButton!
+    @IBOutlet weak var btn2x2: UIButton!
+    @IBOutlet weak var btn2x3: UIButton!
+    @IBOutlet weak var btn3x1: UIButton!
+    @IBOutlet weak var btn3x2: UIButton!
+    @IBOutlet weak var btn3x3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +33,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func actBtn1x1(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func actBtn1x2(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func actBtn1x3(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func actBtn2x1(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func actBtn2x2(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func actBtn2x3(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func actBtn3x1(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func actBtn3x2(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func actBtn3x3(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func actBtnReiniciar(_ sender: UIButton) {
+        
+    }
+    
+    
 }
 
