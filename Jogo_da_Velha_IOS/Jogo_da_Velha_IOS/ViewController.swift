0f7a46304c57bcab304f305dae14d640edce6b1c
Ednilson Rossi <ednilsonrossi@gmail.com>
2018-11-30 20:30:56 -0200

    Projeto vazio

---
+++ Jogo_da_Velha_IOS/Jogo_da_Velha_IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jogo_da_Velha_IOS
+//
+//  Created by pos on 30/11/2018.
+//  Copyright © 2018 pos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
